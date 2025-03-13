4d9b7ba85dc040ed5cbda446487d7ec60c86002c
Royston <roystonad2004@gmail.com>
2025-03-13 23:03:38 +0530

    iniial commit for clerk

---
+++ src/app/api/clerk/webhook/route.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+//  /api/clerk/webhook
+import { db } from "@/server/db";
+
+
+export const POST = async(req:Request)=>{
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const {data} = await req.json();
+    console.log(data);
+    const emailAddress = data.email_addresses[0].email_address;
+    const firstName = data.first_name;
+    const lastName = data.last_name;
+    const image = data.image_url;
+    const id = data.id;
+
+    await db.user.create({
+        data: {
+            emailAddress: emailAddress,
+            firstName: firstName,
+            lastName: lastName,
+            imageUrl: image,
+            id: id
+        }
+    });
+
+    return new Response('Webhook received', {status: 200});
+}
---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher([
+    '/sign-in(.*)',
+    '/sign-up(.*)',
+    '/api/clerk/webhook(.*)', // Add this line to the array of public routes to allow the webhook to bypass the middleware and be accessible by Clerk
+  ])
+  
+  export default clerkMiddleware(async (auth, request) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    if (!isPublicRoute(request)) {
+      await auth.protect()
+    }
+  })
+  
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
+
